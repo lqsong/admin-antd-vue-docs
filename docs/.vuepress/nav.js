d11ca306d4db1f5d6cdfda62e6cf561bb1d1fbc0
lqs <957698457@qq.com>
2021-08-15 20:25:10 +0800

    update logo

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -43,7 +43,7 @@ module.exports = [
             {text: 'admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc'},
             {text: 'admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc'},
             {text: 'electron-admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc/tsv2/guide/senior/electron.html'},
-            {text: 'electron-admin-antd-vue', link: '/guide/senior/electron'},
+            {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/webpackts/guide/senior/electron.html'},
             {text: 'electron-admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc/guide/senior/electron.html'},
         ]
     },
